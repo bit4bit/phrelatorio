b51a5208105537a37364c572cb6a0ba0b6822018
bit4bit <bit4bit@riseup.net>
2022-06-12 00:47:17 +0000

    add test
    FossilOrigin-Name: 18393ea1f0d607047547dea0a06c86b68e3a6eac199e3ce2670da29adbcc24e9

--- tests/PhrelatorioTest.php
+++ tests/PhrelatorioTest.php
@@ -182,4 +182,53 @@ XML;
 $tml = Phrelatorio\OpenDocument::fromString($input);
     $this->assertEquals($wants, $tml->asXML(['items' => ['A', 'B', 'C']]));
     }
+
+    public function testConditionalRow(): void
+    {
+        $input = <<<XML
+<?xml version="1.0"?>
+<document xmlns="http://test" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
+<table:table>
+<table:table-row>
+<table:cell>
+    <text:p><text:a xlink:href="phrelatorio://condition%20show">for "item items"</text:a></text:p>
+</table:cell>
+</table:table-row>
+<table:table-row>
+<table:cell>
+    <text:p>NOT SHOW THIS</text:p>
+</table:cell>
+</table:table-row>
+<table:table-row>
+<table:cell>
+    <text:p><text:a xlink:href="phrelatorio:///condition">/condition</text:a></text:p>
+</table:cell>
+</table:table-row>
+<table:table-row>
+<table:cell>
+    <text:p>SHOW THIS</text:p>
+</table:cell>
+</table:table-row>
+</table:table>
+</document>
+
+XML;
+
+        $wants = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<document xmlns="http://test" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <table:table>
+    <table:table-row>
+      <table:cell>
+        <text:p>SHOW THIS</text:p>
+      </table:cell>
+    </table:table-row>
+  </table:table>
+</document>
+
+XML;
+
+        $tml = Phrelatorio\OpenDocument::fromString($input);
+        $this->assertEquals($wants, $tml->asXML(['show' => false]));
+    }
 }
